ff4eaf426823a78ee54ae741223066cad3fb95e5
Jamie Won <jamie.won.2000@gmail.com>
2022-03-21 23:10:45 -0400

    Allow users to enter decimal or hexadecimal values

--- src/Components/GetInfo.js
+++ src/Components/GetInfo.js
@@ -3,7 +3,7 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import generate_instruction from "../generate_instruction";
-import { Grid, Typography } from '@mui/material';
+import { Grid, MenuItem, Select, Typography } from '@mui/material';
 const GetInfo = () => {
 
     const [instructionDetails, setInstructionDetails] = useState({
@@ -17,13 +17,23 @@ const GetInfo = () => {
         hex: "",
         binary: "",
     });
+    const [cType, setCType] = useState(10);
     const [error, setError] = useState("");
     const handleTextChange = (event, type) => {
         setInstructionDetails({...instructionDetails, [type]: event.target.value || null });
     }
+    const handleCTypeChange = (event) => setCType(event.target.value);
     const handleSubmitInstruction = () => {
         // console.log(instructionDetails)
-        const binary = generate_instruction.binary(instructionDetails[process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_INSTRUCTION], instructionDetails[process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_RA], instructionDetails[process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_RB], instructionDetails[process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_RC], instructionDetails[process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_C])
+        const binary = generate_instruction.binary(
+            instructionDetails[process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_INSTRUCTION], 
+            instructionDetails[process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_RA], 
+            instructionDetails[process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_RB], 
+            instructionDetails[process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_RC], 
+            (cType === 10) ? 
+                instructionDetails[process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_C] : 
+                parseInt(instructionDetails[process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_C], 16)
+            )
         if (binary.error) {
             setError(binary.error);
         } else {
@@ -66,16 +76,24 @@ const GetInfo = () => {
                         error={isNaN(instructionDetails[process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_RC])} 
                         helperText={isNaN(instructionDetails[process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_RC]) ? process.env.REACT_APP_GETINFO_REGISTER_INPUT_ERROR : ""}/>
                 </Grid>
-                <Grid item xs={12} >
+                <Grid item xs={8} md={10} >
                     <TextField fullWidth 
                         label={process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_C} 
                         onChange={(event) => handleTextChange(event, process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_C)} 
-                        error={instructionDetails[process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_C] && isNaN(instructionDetails[process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_C])} 
-                        helperText={isNaN(instructionDetails[process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_C]) ? process.env.REACT_APP_GETINFO_REGISTER_INPUT_ERROR : ""}/>
+                        error={cType !== 16 && instructionDetails[process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_C] && isNaN(instructionDetails[process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_C])} 
+                        helperText={(cType !== 16 && isNaN(instructionDetails[process.env.REACT_APP_GETINFO_INSTRUCTION_DETAILS_C]) ? process.env.REACT_APP_GETINFO_REGISTER_INPUT_ERROR : "") || cType === 16 ? process.env.REACT_APP_GETINFO_INSTRUCTION_INVALID_C : ""}/>
+                </Grid>
+                <Grid item xs={4} md={2}>
+                    <Select value={cType} onChange={handleCTypeChange} fullWidth>
+                        <MenuItem value={10}>Decimal</MenuItem>
+                        <MenuItem value={16}>Hexadecimal</MenuItem>
+                    </Select>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button onClick={handleSubmitInstruction} variant="outlined">Submit Instruction</Button> 
                 </Grid>
             </Grid>
-           
-            <Button onClick={handleSubmitInstruction}>Submit Instruction</Button> 
+
             <br/>
             <Grid container spacing={2}>
                 <Grid item lg={1} xl={2}/>
